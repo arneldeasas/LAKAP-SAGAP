0f7f6bb1fc914ce98eea1044bdfea2b5b681eaa2
inicolas <106856208+na14n@users.noreply.github.com>
2024-11-01 20:39:42 +0800

    [BlazorServer](update) Added General UI and Improvements

---
+++ LAKAPSAGAP.BlazorServer/Pages/Stocks/Stocks.razor.cs
@@ -0,0 +1,11 @@
+﻿namespace LAKAPSAGAP.BlazorServer.Pages.Stocks
+{
+    public partial class Stocks
+    {
+        private List<BreadcrumbViewModel> Breadcrumbs = new()
+        {
+            new BreadcrumbViewModel { Path = "javascript:void(0);", Text = "Warehouse" },
+            new BreadcrumbViewModel { Path = "/Warehouse/Stocks", Text = "Stocks" },
+        };
+    }
+}

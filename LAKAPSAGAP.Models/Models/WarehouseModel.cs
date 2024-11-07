9998ad6b1eb00806edf3370d2877af6106828650
inicolas <106856208+na14n@users.noreply.github.com>
2024-11-07 22:22:09 +0800

    [BlazorServer][Models](update) Warehouse Creation Form

--- LAKAPSAGAP.Models/Models/WarehouseModel.cs
+++ LAKAPSAGAP.Models/Models/WarehouseModel.cs
@@ -8,9 +8,9 @@ namespace LAKAPSAGAP.Models.Models
 {
     public class Warehouse : CommonModel
     {
-      
         public string Name { get; set; }
         public string Location { get; set; }
-        public List<ReliefReceived> ReliefReceivedList { get; set; }
+        public List<ReliefReceived> ReliefReceivedList { get; set; } = new();
+        public List<Floor> Floors { get; set; } = new();
     }
 }

7d7de14a62cd7fdbdad9f8a04548e6a8a00d70c3
arneldeasas <113487190+arneldeasas@users.noreply.github.com>
2024-11-14 20:11:22 +0800

    (add) kitting repository

--- LAKAPSAGAP.Models/ViewModels/KitViewModel.cs
+++ LAKAPSAGAP.Models/ViewModels/KitViewModel.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LAKAPSAGAP.Models.Models;
 
-namespace LAKAPSAGAP.Models.Models
+namespace LAKAPSAGAP.Models.ViewModels
 {
-	public class Kit:CommonModel
+	public class KitViewModel 
 	{
 		public string Name { get; set; }
 		public string? Description { get; set; }
@@ -15,17 +16,11 @@ namespace LAKAPSAGAP.Models.Models
 	}
 
 	// component of a Kit
-	public class KitComponent : CommonModel
+	public class KitComponentViewModel
 	{
 		public string ItemId { get; set; }
 		[ForeignKey(nameof(ItemId))]
 		public StockItem StockItem { get; set; }
-		public int Quantity  { get; set; }
-	}
-
-	public enum KitType
-	{
-		FoodKit = 0,
-		NonFoodKit = 1
+		public int Quantity { get; set; }
 	}
 }
---
+++ LAKAPSAGAP.Services/Repositories/IKittingRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAKAPSAGAP.Services.Repositories
+{
+	public interface IKittingRepository
+	{
+		public Task<Kit> CreateKit(KitViewModel kitViewModel);
+		public Task<Kit> UpdateKit(KitViewModel kitViewModel);
+
+	}
+}

6a3e090665a2dd5ed00ef16e4963f5eddd0f270f
Charles Maverick Herrera <90278189+CMH7@users.noreply.github.com>
2024-11-17 16:24:53 +0800

    refactor: code cleanup and gitignore update

--- LAKAPSAGAP.Services/Core/UoMRepository.cs
+++ LAKAPSAGAP.Services/Core/UoMRepository.cs
@@ -5,91 +5,90 @@ namespace LAKAPSAGAP.Services.Core;
 
 public class UoMRepository : IUoMRepository
 {
-    readonly MyDbContext _context;
+	readonly MyDbContext _context;
 
-    public UoMRepository(MyDbContext context) => _context = context;
+	public UoMRepository(MyDbContext context) => _context = context;
 
-    public async Task<string?> CreateUoM(UoMViewModel uoM)
-    {
-        using IDbContextTransaction transaction = _context.Database.BeginTransaction();
-        string? newUoMId = null;
-
-        try
-        {
-            int uomCount = await _context.GetCount<UoM>();
-            string uomId = IdGenerator.GenerateId(IdGenerator.PFX_UOM, uomCount);
-
-            UoM newUom = new()
-            {
-                Id = uomId,
-                Name = uoM.Name,
-                Symbol = uoM.Symbol
-            };
-
-            await _context.Create<UoM>(newUom);
-            await transaction.CommitAsync();
-            newUoMId = uomId;
-        }
-        catch (Exception)
-        {
-            await transaction.RollbackAsync();
-        }
-
-        return newUoMId;
-    }
-
-    public async Task<List<UoM>> GetAllUoM()
-    {
+	public async Task<string?> CreateUoM(UoMViewModel uoM)
+	{
 		using IDbContextTransaction transaction = _context.Database.BeginTransaction();
-        List<UoM> uomList = [];
-
-		try
-        {
-            uomList = await _context.GetAll<UoM>();
-        }
-        catch (Exception)
-        {
-			await transaction.RollbackAsync();
-        }
-
-        return uomList;
-    }
-
-    public async Task<UoM?> GetUoM(string uomId)
-    {
-		using IDbContextTransaction transaction = _context.Database.BeginTransaction();
-        UoM? uom = null;
+		string? newUoMId = null;
 
 		try
 		{
-			uom = await _context.GetById<UoM>(uomId);
+			int uomCount = await _context.GetCount<UoM>();
+			string uomId = IdGenerator.GenerateId(IdGenerator.PFX_UOM, uomCount);
+
+			UoM newUom = new()
+			{
+				Id = uomId,
+				Name = uoM.Name,
+				Symbol = uoM.Symbol,
+				isArchived = uoM.isArchived
+			};
+
+			await _context.Create<UoM>(newUom);
+
+			await transaction.CommitAsync();
+			newUoMId = uomId;
 		}
 		catch (Exception)
 		{
 			await transaction.RollbackAsync();
 		}
 
-		return uom;
+		return newUoMId;
 	}
 
-    public async Task<bool> UpdateUoM(UoMViewModel uoM)
-    {
+	public async Task<List<UoM>> GetAllUoM()
+	{
+		try
+		{
+			List<UoM> uomList = [];
+			uomList = await _context.GetAll<UoM>();
+			return uomList;
+		}
+		catch (Exception)
+		{
+			return [];
+		}
+	}
+
+	public async Task<UoM?> GetUoM(string uomId)
+	{
+		try
+		{
+			UoM? uom = null;
+			uom = await _context.GetById<UoM>(uomId);
+			return uom;
+		}
+		catch (Exception)
+		{
+			return null;
+		}
+	}
+
+	public async Task<bool> UpdateUoM(UoMViewModel uoM)
+	{
 		using IDbContextTransaction transaction = _context.Database.BeginTransaction();
 		try
 		{
 			UoM? uom = await _context.GetById<UoM>(uoM.Id);
-            if (uom is null) return false;
+			if (uom is null) return false;
 
-            uom.Name = uoM.Name;
-            uom.Symbol = uoM.Symbol;
+			uom.Name = uoM.Name;
+			uom.Symbol = uoM.Symbol;
+			uom.isArchived = uoM.isArchived;
 
-            await _context.UpdateItem(uom);
-            return true;
+			await _context.UpdateItem(uom);
+
+			await transaction.CommitAsync();
+			return true;
 		}
 		catch (Exception)
 		{
 			await transaction.RollbackAsync();
-            return false;
+			return false;
 		}
 	}
 }

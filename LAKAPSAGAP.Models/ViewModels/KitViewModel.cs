9b4bd8a46f32998595acabd919427909c16a9e05
inicolas <na14an.07@gmail.com>
2024-11-24 22:16:51 +0800

    [BlazorServer](fix) Kit View Model and Datagrid Management

--- LAKAPSAGAP.Models/ViewModels/KitViewModel.cs
+++ LAKAPSAGAP.Models/ViewModels/KitViewModel.cs
@@ -1,27 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using LAKAPSAGAP.Models.Models;
+﻿using LAKAPSAGAP.Models.Models;
 
-namespace LAKAPSAGAP.Models.ViewModels
+namespace LAKAPSAGAP.Models.ViewModels;
+
+public class KitViewModel
 {
-	public class KitViewModel 
-	{
-		public string Id { get; set; }
-		public string Name { get; set; }
-		public string? Description { get; set; }
-		public KitType KitType { get; set; }
-		public List<KitComponentViewModel> KitComponentList { get; set; }
-	}
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string? Description { get; set; }
+    public DateTime ExpiryDate { get; set; }
+    public DateTime ManufactureDate { get; set; }
+    public RackViewModel Location { get; set; }
+    public KitType KitType { get; set; } // Kit Type is either Food or Non-Food
+    public List<KitComponentViewModel> KitComponentList { get; set; } // Items sa loob ng isang Kit
+}
 
-	// component of a Kit
-	public class KitComponentViewModel
-	{
-		public string Id { get; set; }
-		public string ItemId { get; set; }
-		public string ItemName { get; set; }
-		public int Quantity { get; set; }
-	}
+// component of a Kit <== kung anu man yung nasa Item Master Data
+public class KitComponentViewModel
+{
+    public string Id { get; set; }
+    public string ItemId { get; set; }
+    public string ItemName { get; set; }
+    public int Quantity { get; set; }
+    public int QuantityOnHand { get; set; }
+    public int Status { get; set; }
 }

7572193c43476a7e0befa17e906b297c6c9a69a2
inicolas <na14an.07@gmail.com>
2024-11-28 00:26:21 +0800

    feat: Kits Availability Table

--- LAKAPSAGAP.BlazorServer/Pages/Kits/KitsAvailability.razor.cs
+++ LAKAPSAGAP.BlazorServer/Pages/Kits/KitsAvailability.razor.cs
@@ -1,7 +1,10 @@
-﻿namespace LAKAPSAGAP.BlazorServer.Pages.Kits
-{
-	public partial class KitsAvailability
-	{
+﻿using static LAKAPSAGAP.Models.Models.Kit;
 
-	}
+namespace LAKAPSAGAP.BlazorServer.Pages.Kits;
+
+public partial class KitsAvailability
+{
+
+    List<KitViewModel> KitList { get; set; } = new List<KitViewModel>();   
+
 }

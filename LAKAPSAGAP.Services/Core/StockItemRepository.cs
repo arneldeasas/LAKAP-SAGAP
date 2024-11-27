f7147d5def2cc4bb7d67b0ea206c48357f489291
Charles Maverick Herrera <90278189+CMH7@users.noreply.github.com>
2024-11-27 23:07:19 +0800

    fix: removed boilerplates in code

--- LAKAPSAGAP.Services/Core/StockItemRepository.cs
+++ LAKAPSAGAP.Services/Core/StockItemRepository.cs
@@ -1,91 +1,85 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using LAKAPSAGAP.Services.Core.Helpers;
+﻿using LAKAPSAGAP.Services.Core.Helpers;
 
-namespace LAKAPSAGAP.Services.Core
+namespace LAKAPSAGAP.Services.Core;
+
+public class StockItemRepository : IStockItemRepository
 {
-	public class StockItemRepository :IStockItemRepository
+	readonly MyDbContext _context;
+	StockItemRepository(MyDbContext context)
 	{
-		readonly MyDbContext _context;
-		StockItemRepository(MyDbContext context)
+		_context = context;
+	}
+	public async Task<string?> CreateStockItem(StockItemViewModel stockItemVM)
+	{
+
+		try
 		{
-			_context = context;
+			int count = await _context.GetCount<StockItem>();
+			string Id = IdGenerator.GenerateId(IdGenerator.PFX_STOCKITEM, count);
+
+			var newStockItem = await _context.Create<StockItem>(new()
+			{
+				Id = Id,
+				Name = stockItemVM.Name,
+				CategoryId = stockItemVM.CategoryId,
+				UoMId = stockItemVM.UoMId,
+				isArchived = stockItemVM.isArchived
+			});
+
+			return newStockItem.Id;
 		}
-		public async Task<string?> CreateStockItem(StockItemViewModel stockItemVM)
+		catch (Exception)
 		{
-			
-			try
-			{
-				int count = await _context.GetCount<StockItem>();
-				string Id = IdGenerator.GenerateId(IdGenerator.PFX_STOCKITEM, count);
-
-				var newStockItem = await _context.Create<StockItem>(new()
-				{
-					Id=Id,
-					Name = stockItemVM.Name,
-					CategoryId = stockItemVM.CategoryId,
-					UoMId = stockItemVM.UoMId,
-					isArchived = stockItemVM.isArchived
-				});
-
-				return newStockItem.Id;
-			}
-			catch (Exception)
-			{
-				return null;
-			}
-
+			return null;
 		}
 
-		public async Task<List<StockItem>> GetAllStockItem()
-		{
-			try
-			{
-				List<StockItem> stockItemList = [];
-				stockItemList = await _context.GetAllNotDeleted<StockItem>();
-				return stockItemList;
-			}
-			catch (Exception)
-			{
+	}
 
-				return [];
-			}
-		}
-		public async Task<StockItem?> GetStockItem(string stockItemID)
+	public async Task<List<StockItem>> GetAllStockItem()
+	{
+		try
 		{
-			try
-			{
-				StockItem? stockItem = null;
-				stockItem = await _context.GetByIdIncludeArchivedsOnly<StockItem>(stockItemID);
-				return stockItem;
-			}
-			catch (Exception)
-			{
-				return null;
-			}
+			List<StockItem> stockItemList = [];
+			stockItemList = await _context.GetAllNotDeleted<StockItem>();
+			return stockItemList;
 		}
-		public async Task<bool> UpdateStockItem(StockItemViewModel stockItemVM)
+		catch (Exception)
 		{
-			try
-			{
-				StockItem? stockItem = await _context.GetById<StockItem>(stockItemVM.Id);
-				if (stockItem == null) return false;
-			
-				stockItem.Name = stockItemVM.Name;
-				stockItem.CategoryId = stockItem.CategoryId;
-				stockItem.UoMId = stockItemVM.UoMId;
-				stockItem.isArchived = stockItemVM.isArchived;
 
-				await _context.UpdateItem<StockItem>(stockItem);
-				return true;
-			}
-			catch (Exception)
-			{
-				return false;
-			}
+			return [];
+		}
+	}
+	public async Task<StockItem?> GetStockItem(string stockItemID)
+	{
+		try
+		{
+			StockItem? stockItem = null;
+			stockItem = await _context.GetByIdIncludeArchivedsOnly<StockItem>(stockItemID);
+			return stockItem;
+		}
+		catch (Exception)
+		{
+			return null;
+		}
+	}
+	public async Task<bool> UpdateStockItem(StockItemViewModel stockItemVM)
+	{
+		try
+		{
+			StockItem? stockItem = await _context.GetById<StockItem>(stockItemVM.Id);
+			if (stockItem == null) return false;
+
+			stockItem.Name = stockItemVM.Name;
+			stockItem.CategoryId = stockItem.CategoryId;
+			stockItem.UoMId = stockItemVM.UoMId;
+			stockItem.isArchived = stockItemVM.isArchived;
+
+			await _context.UpdateItem<StockItem>(stockItem);
+			return true;
+		}
+		catch (Exception)
+		{
+			return false;
 		}
 	}
 }
--- LAKAPSAGAP.Services/Repositories/IStockItemRepository.cs
+++ LAKAPSAGAP.Services/Repositories/IStockItemRepository.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace LAKAPSAGAP.Services.Repositories;
 
-namespace LAKAPSAGAP.Services.Repositories
+public interface IStockItemRepository
 {
-	public interface IStockItemRepository
-	{
-		Task<string?> CreateStockItem(StockItemViewModel stockItemVM);
-		Task<StockItem?> GetStockItem(string stockItemID);
-		Task<List<StockItem>> GetAllStockItem();
-		Task<bool> UpdateStockItem(StockItemViewModel stockItemVM);
-	}
+	Task<string?> CreateStockItem(StockItemViewModel stockItemVM);
+	Task<StockItem?> GetStockItem(string stockItemID);
+	Task<List<StockItem>> GetAllStockItem();
+	Task<bool> UpdateStockItem(StockItemViewModel stockItemVM);
 }

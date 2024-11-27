3196041390b0de00d38f9e3317c6fe93d555bca4
inicolas <na14an.07@gmail.com>
2024-11-28 00:26:57 +0800

    feat: Kits Creation Form

--- LAKAPSAGAP.BlazorServer/Pages/Kits/KitsCreateUpdate.razor.cs
+++ LAKAPSAGAP.BlazorServer/Pages/Kits/KitsCreateUpdate.razor.cs
@@ -4,18 +4,23 @@ namespace LAKAPSAGAP.BlazorServer.Pages.Kits;
 public partial class KitsCreateUpdate
 {
 
-    bool Loading = true;
+    [Inject] DialogService _dialogService { get; set; }
+    [Inject] protected IJSRuntime _jSRuntime { get; set; } = default!;
 
-    public KitViewModel Kit { get; set; }
+    List<KitViewModel> Kits { get; set; } = new();
+    public KitViewModel Kit { get; set; } = new();
+    public List<ReliefReceivedViewModel> Items { get; set; } = new();
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
         if (firstRender)
         {
-            Loading = false;
-            StateHasChanged();
         }
     }
 
+    Task SubmitKit()
+    {
+        return Task.CompletedTask;
+    }
 
 }

d609c78f8638546a691e8aa2d44552017cfea6dd
Charles Maverick Herrera <90278189+CMH7@users.noreply.github.com>
2024-11-28 03:21:30 +0800

    fix: removed unecessary properties of Floor and Stock Item

--- LAKAPSAGAP.Models/Models/StockItem.cs
+++ LAKAPSAGAP.Models/Models/StockItem.cs
@@ -2,13 +2,11 @@
 
 public class StockItem : CommonModel //record of all Relief Items
 {
+	public string Name { get; set; }
 	public string CategoryId { get; set; }
 	public Category Category { get; set; }
-	public string Name { get; set; }
 	public string UoMId { get; set; }
 	public UoM UoM { get; set; }
 
-	//public string FloorId { get; set; } //StockDetail is the one that has rackId 
-	//public Floor Floor { get; set; }
 	public List<StockDetail> StockDetailList { get; set; }
 }

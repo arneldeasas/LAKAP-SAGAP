d37b65b7bea45458fe29ba0e17bcefac7b9a49ed
arneldeasas <113487190+arneldeasas@users.noreply.github.com>
2024-11-30 00:00:55 +0800

    Update RequestReliefModel.cs

--- LAKAPSAGAP.Models/Models/RequestReliefModel.cs
+++ LAKAPSAGAP.Models/Models/RequestReliefModel.cs
@@ -12,6 +12,13 @@ namespace LAKAPSAGAP.Models.Models
 		public RequestReason Reason { get; set; }
 		public string SpecificReason { get; set; }
 		public string Organization { get; set; } // barangay/organization
+		public List<Request> RequestList { get; set; }
+		public DateTime TargetDateToReceive { get; set; }
+		public string ReceiverAddress { get; set; } //Address of evacuation post
+		public string AdditionalNotes { get; set; } //for the statement of specific reason
+		public string ReceiverName { get; set; } //Name of the person who will receive the relief goods
+		public int ContactNumber { get; set; } //Contact number of the person who will receive the relief goods
+		public List<RequestAttachment> AttachmentList { get; set; }
 	}
 
 	public enum RequestReason
@@ -20,8 +27,24 @@ namespace LAKAPSAGAP.Models.Models
 		Noncalamity =2
 	}
 
-	public class ReliefRequest
+	public class Request:CommonModel
 	{
+		public string ReliefRequestId { get; set; }
+		public string UnitId { get; set; } // id of either kit or item
+		public RequestType RequestType { get; set; }
+		public int Quantity { get; set; }	
+		public ReliefRequestDetail ReliefRequest { get; set; }
+	}
+	public enum RequestType
+	{
+		Kit = 1,
+		Item = 2
+	}
+	public class RequestAttachment:CommonModel
+	{
+		public string ReliefRequestId { get; set; }
+		public string Url { get; set; }
+		public ReliefRequestDetail ReliefRequest { get; set; }
 
 	}
 }

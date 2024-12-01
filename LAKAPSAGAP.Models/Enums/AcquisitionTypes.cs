2dce260a5aa84e1fccaec350b9891416245cb3b8
Charles Maverick Herrera <90278189+CMH7@users.noreply.github.com>
2024-12-01 14:09:20 +0800

    feat: added acquisition types

---
+++ LAKAPSAGAP.Models/Enums/AcquisitionTypes.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace LAKAPSAGAP.Models.Enums;
+
+public enum AcquisitionTypes
+{
+	[Description("Purchased")]
+	PURCHASED,
+
+	[Description("Donation")]
+	DONATION
+}

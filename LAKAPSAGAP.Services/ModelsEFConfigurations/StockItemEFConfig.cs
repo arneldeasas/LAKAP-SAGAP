196e748e92f7d358fc085cf7ce70474627cf7911
Charles Maverick Herrera <90278189+CMH7@users.noreply.github.com>
2024-12-02 03:00:09 +0800

    feat: separated ef configurations for each entity for more readable and organized code

---
+++ LAKAPSAGAP.Services/ModelsEFConfigurations/StockItemEFConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace LAKAPSAGAP.Services.ModelsEFConfigurations;
+
+public static class StockItemEFConfig
+{
+	public static void OnModelCreation(ModelBuilder builder)
+	{
+		builder.Entity<StockItem>().HasOne(r => r.Category).WithMany(x => x.StockItems).HasForeignKey(r => r.CategoryId).OnDelete(DeleteBehavior.NoAction);
+		builder.Entity<StockItem>().HasOne(r => r.UoM).WithMany(x => x.StockItems).HasForeignKey(r => r.UoMId).OnDelete(DeleteBehavior.NoAction);
+		builder.Entity<StockItem>().HasMany(r => r.StockDetailList).WithOne(x => x.Item).HasForeignKey(r => r.ItemId).OnDelete(DeleteBehavior.NoAction);
+	}
+}

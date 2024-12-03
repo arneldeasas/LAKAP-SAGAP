98dde7c1dbbeffbed31f8760983d09d83797cb8f
Charles Maverick Herrera <90278189+CMH7@users.noreply.github.com>
2024-12-04 00:18:36 +0800

    fix: removed access modifier

--- LAKAPSAGAP.Services/Repositories/IReliefReceivedRepository.cs
+++ LAKAPSAGAP.Services/Repositories/IReliefReceivedRepository.cs
@@ -4,13 +4,13 @@ namespace LAKAPSAGAP.Services.Repositories;
 
 public interface IReliefReceivedRepository
 {
-	public Task<ReliefReceived> CreateReliefReceived(ReliefReceivedViewModel warehouseViewModel);
-	public Task<bool> UpdateReliefReceived(ReliefReceivedViewModel warehouseViewModel);
-	public Task<ReliefReceived> DeleteReliefReceived(string Id);
-	public Task<ReliefReceived> ArchiveReliefReceived(string Id);
-	public Task<List<ReliefReceived>> GetAllReliefReceived();
-	public Task<ReliefReceivedFormSelections> GetAllInitialSelectionOptions(ReliefReceivedViewModel reliefReceivedViewModel); // will get all initial options/records for the selection inputs, includes stocktype, stockcategory, uom, and floor
-	public Task<List<Floor>> GetAllFloorsActive();
-	public Task<List<Floor>> GetAllFloorsActiveBasedOnWarehouse(string whseId);
-	public Task<List<Rack>> GetAllRacksBasedOnFloor(string floorId);
+	Task<ReliefReceived> CreateReliefReceived(ReliefReceivedViewModel warehouseViewModel);
+	Task<bool> UpdateReliefReceived(ReliefReceivedViewModel warehouseViewModel);
+	Task<ReliefReceived> DeleteReliefReceived(string Id);
+	Task<ReliefReceived> ArchiveReliefReceived(string Id);
+	Task<List<ReliefReceived>> GetAllReliefReceived();
+	Task<ReliefReceivedFormSelections> GetAllInitialSelectionOptions(ReliefReceivedViewModel reliefReceivedViewModel); // will get all initial options/records for the selection inputs, includes stocktype, stockcategory, uom, and floor
+	Task<List<Floor>> GetAllFloorsActive();
+	Task<List<Floor>> GetAllFloorsActiveBasedOnWarehouse(string whseId);
+	Task<List<Rack>> GetAllRacksBasedOnFloor(string floorId);
 }

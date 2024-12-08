884b3ede88c6f0e3b40ae6342db392dbf49d2f24
Charles Maverick Herrera <90278189+CMH7@users.noreply.github.com>
2024-12-08 23:11:33 +0800

    fix: updated the last 2 columns at the bottom of the dashboard

--- LAKAPSAGAP.BlazorServer/Controls/Dashboard/DashboardControl.razor.cs
+++ LAKAPSAGAP.BlazorServer/Controls/Dashboard/DashboardControl.razor.cs
@@ -1,95 +1,148 @@
-using System.Globalization;
-
 namespace LAKAPSAGAP.BlazorServer.Controls.Dashboard;
 
 public partial class DashboardControl
 {
-    List<PendingReliefRequest> _pendingReliefRequestsToday { get; set; }
-    List<ReleasedOfReliefGoods> _releasedOfReliefGoodsPerMonth { get; set; }
+	List<PendingReliefRequest> _pendingReliefRequestsToday { get; set; }
+	List<ReleasedOfReliefGoods> _releasedOfReliefGoodsPerMonth { get; set; }
+	List<ReceivedDonationsByBarangay> _receivedDonationsByBarangayList { get; set; }
 
-    int _selectedYear { get; set; }
+	int _selectedYear { get; set; }
 
 	protected override void OnInitialized()
 	{
-        _pendingReliefRequestsToday ??= [
-            new(){
-                RequestorName = "Kap. Eduardo Jr.",
-                RequestorEmail = "arneldeasas@gmail.com",
-                Barangay = "Karuhatan",
-                KitName = "Food Pack",
-                MainReason = "Fire Incident",
-                RequestDate = DateTime.Now,
-            },
-            new(){
-                RequestorName = "CSWD Admin Ernesto",
-                RequestorEmail = "naian@gmail.com",
-                Barangay = "Dalandanan",
-                KitName = "Family Kit",
-                MainReason = "Flood Incident",
-                RequestDate = DateTime.Now,
-            },
-            new(){
-                RequestorName = "CSWD Admin Soledad",
-                RequestorEmail = "charlesm.herrera0700@gmail.com",
-                Barangay = "Manotoc",
-                KitName = "Food Pack",
-                MainReason = "Bahay Kalinga",
-                RequestDate = DateTime.Now,
-            }];
+		_pendingReliefRequestsToday ??= [
+			new(){
+				RequestorName = "Kap. Eduardo Jr.",
+				RequestorEmail = "arneldeasas@gmail.com",
+				Barangay = "Karuhatan",
+				KitName = "Food Pack",
+				MainReason = "Fire Incident",
+				RequestDate = DateTime.Now,
+			},
+			new(){
+				RequestorName = "CSWD Admin Ernesto",
+				RequestorEmail = "naian@gmail.com",
+				Barangay = "Dalandanan",
+				KitName = "Family Kit",
+				MainReason = "Flood Incident",
+				RequestDate = DateTime.Now,
+			},
+			new(){
+				RequestorName = "CSWD Admin Soledad",
+				RequestorEmail = "charlesm.herrera0700@gmail.com",
+				Barangay = "Manotoc",
+				KitName = "Food Pack",
+				MainReason = "Bahay Kalinga",
+				RequestDate = DateTime.Now,
+			}];
 
-        _releasedOfReliefGoodsPerMonth ??= [
-            new(){
+		_releasedOfReliefGoodsPerMonth ??= [
+			new(){
 				ReleasedDate = new DateTime(2023, 1, 1),
 				Total = 69,
-            },
-            new(){
-                ReleasedDate = new DateTime(2023, 2, 1),
-                Total = 32,
-            },
-            new(){
-                ReleasedDate = new DateTime(2023, 3, 1),
-                Total = 109,
-            },
-            new(){
-                ReleasedDate = new DateTime(2023, 4, 1),
-                Total = 37,
-            },
-            new(){
-                ReleasedDate = new DateTime(2023, 5, 1),
-                Total = 45,
-            },
-            new(){
-                ReleasedDate = new DateTime(2023, 6, 1),
-                Total = 91,
-            },
-            new(){
-                ReleasedDate = new DateTime(2023, 7, 1),
-                Total = 60,
-            },
-            new(){
-                ReleasedDate = new DateTime(2023, 8, 1),
-                Total = 116,
-            },
-            new(){
-                ReleasedDate = new DateTime(2023, 9, 1),
-                Total = 73,
-            },
-            new(){
-                ReleasedDate = new DateTime(2023, 10, 1),
-                Total = 67,
-            },
-            new(){
-                ReleasedDate = new DateTime(2023, 11, 1),
-                Total = 82,
-            },
-            new(){
-                ReleasedDate = new DateTime(2023, 12, 1),
-                Total = 62,
-            },
-            new(){
-                ReleasedDate = new DateTime(2024, 1, 1),
-                Total = 62,
-            }];
+			},
+			new(){
+				ReleasedDate = new DateTime(2023, 2, 1),
+				Total = 32,
+			},
+			new(){
+				ReleasedDate = new DateTime(2023, 3, 1),
+				Total = 109,
+			},
+			new(){
+				ReleasedDate = new DateTime(2023, 4, 1),
+				Total = 37,
+			},
+			new(){
+				ReleasedDate = new DateTime(2023, 5, 1),
+				Total = 45,
+			},
+			new(){
+				ReleasedDate = new DateTime(2023, 6, 1),
+				Total = 91,
+			},
+			new(){
+				ReleasedDate = new DateTime(2023, 7, 1),
+				Total = 60,
+			},
+			new(){
+				ReleasedDate = new DateTime(2023, 8, 1),
+				Total = 116,
+			},
+			new(){
+				ReleasedDate = new DateTime(2023, 9, 1),
+				Total = 73,
+			},
+			new(){
+				ReleasedDate = new DateTime(2023, 10, 1),
+				Total = 67,
+			},
+			new(){
+				ReleasedDate = new DateTime(2023, 11, 1),
+				Total = 82,
+			},
+			new(){
+				ReleasedDate = new DateTime(2023, 12, 1),
+				Total = 62,
+			},
+			new(){
+				ReleasedDate = new DateTime(2024, 1, 1),
+				Total = 62,
+			}];
+
+		_receivedDonationsByBarangayList ??= [
+			new(){
+				BarangayName="Arkong Bato",
+				Total=17
+			},
+			new(){
+				BarangayName="Bagbaguin",
+				Total=42
+			},
+			new(){
+				BarangayName="Balangkas",
+				Total=21
+			},
+			new(){
+				BarangayName="Bignay",
+				Total=13
+			},
+			new(){
+				BarangayName="Bisig",
+				Total=27
+			},
+			new(){
+				BarangayName="Canumay East",
+				Total=37
+			},
+			new(){
+				BarangayName="Coloong",
+				Total=34
+			},
+			new(){
+				BarangayName="Dalandanan",
+				Total=22
+			},
+			new(){
+				BarangayName="Gen T. De Leon",
+				Total=10
+			},
+			new(){
+				BarangayName="Isla",
+				Total=6
+			},
+			new(){
+				BarangayName="Karuhatan",
+				Total=107
+			},
+			new(){
+				BarangayName="Lawang Bato",
+				Total=29
+			},
+			new(){
+				BarangayName="Malinta",
+				Total=21
+			}];
 
 		_selectedYear = _releasedOfReliefGoodsPerMonth.DistinctBy(x => x.ReleasedDate.Year).First().ReleasedDate.Year;
 		base.OnInitialized();
@@ -102,19 +155,24 @@ public partial class DashboardControl
 
 	class PendingReliefRequest
 	{
-        public string RequestorName { get; set; }
-        public string? RequestorEmail { get; set; }
-        public string Image { get; set; }
-        public string Barangay { get; set; }
-        public string KitName { get; set; }
-        public string MainReason { get; set; }
-        public DateTime RequestDate { get; set; }
-    }
+		public string RequestorName { get; set; }
+		public string? RequestorEmail { get; set; }
+		public string Image { get; set; }
+		public string Barangay { get; set; }
+		public string KitName { get; set; }
+		public string MainReason { get; set; }
+		public DateTime RequestDate { get; set; }
+	}
 
-    class ReleasedOfReliefGoods
-    {
-        public DateTime ReleasedDate { get; set; }
-        public double Total { get; set; }
-    }
+	class ReleasedOfReliefGoods
+	{
+		public DateTime ReleasedDate { get; set; }
+		public double Total { get; set; }
+	}
 
+	class ReceivedDonationsByBarangay
+	{
+		public string BarangayName { get; set; }
+		public int Total { get; set; }
+	}
 }
